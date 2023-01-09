b2ee6e71ef8c2350616a73b818fbceac291907ea
fandi ahmad <superdev195128@hotmail.com>
2023-01-09 21:52:48 +0900

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,14 +8,14 @@
         <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link rel="shortcut icon" href="{{ asset('/images/favicon.ico')}}">
 
 		<!-- App css -->
-		<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bs-stylesheet" />
-		<link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-stylesheet" />
+		<link href="{{ asset('/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bs-stylesheet" />
+		<link href="{{ asset('/css/app.min.css')}}" rel="stylesheet" type="text/css" id="app-stylesheet" />
 
 		<!-- icons -->
-		<link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+		<link href="{{ asset('/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
 
     </head>
 
@@ -112,10 +112,10 @@
         <!-- end page -->
 
         <!-- Vendor js -->
-        <script src="assets/js/vendor.min.js"></script>
+        <script src="{{ asset('/js/vendor.min.js ')}}"></script>
 
         <!-- App js -->
-        <script src="assets/js/app.min.js"></script>
+        <script src="{{ asset('/js/app.min.js')}}"></script>
         
     </body>
 </html>
