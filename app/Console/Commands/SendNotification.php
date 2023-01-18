05ddeebe5de65caa280e5bac508719e97b4f19d3
fandi ahmad <superdev195128@hotmail.com>
2023-01-18 11:55:01 +0900

    update

--- app/Console/Commands/SendNotification.php
+++ app/Console/Commands/SendNotification.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 
 use App\Models\User;
+use App\Models\Url;
 use App\Models\Notification;
 use App\Models\NotificationService;
 
@@ -28,9 +29,12 @@ class SendNotification extends Command
      */
     protected $description = 'Command description';
 
+    public const SENT_COUNT = 2;
+    protected $count = 1;
     protected $lower_price;
     protected $upper_price;
     protected $excluded_word;
+    protected $user;
 
     /**
      * Create a new command instance.
@@ -53,8 +57,10 @@ class SendNotification extends Command
 
         $availableUsers = User::where('is_admin',0)->where('active',1)->get();
         foreach($availableUsers as $user) {
+            $this->user = $user;
             $notifications = $user->notifications;
             foreach($notifications as $notification) {
+                $this->count = 1;
                 $keyword = $notification->keyword;
                 $this->lower_price = $notification->lower_price;
                 $this->upper_price = $notification->upper_price;
@@ -63,13 +69,13 @@ class SendNotification extends Command
                 $status = $notification->status;
 
                 foreach($services as $service) {
-                    
+                    if($this->count > self::SENT_COUNT) break;
                     if (str_contains($service, 'wowma')) {
                         $new = mb_convert_encoding($keyword, "SJIS", "UTF-8");
                         $totalPages = 1;
 
                         for($i = 0; $i < $totalPages; $i++) {
-                            
+                            if($this->count > self::SENT_COUNT) break;
                             $response = Http::get('https://wowma.jp/catalog/api/search/items', [
                                 'keyword' => $new,
                                 'e_scope' => 'O',
@@ -88,16 +94,17 @@ class SendNotification extends Command
                             }
                             $hitItems = $response->object()->hitItems;
                             foreach($hitItems as $item) {
-                                
+                                if($this->count > self::SENT_COUNT) break;
                                 if($this->compareCondition($this->lower_price, $this->upper_price,$this->excluded_word, $item->currentPrice, $item->itemName )){
                                     $content = '';
-                                    $content .= $user->name.' 様'.PHP_EOL;
-                                    $content .= '新商品があります。'.PHP_EOL;
-                                    $content .= '商品名　'.$item->itemName.PHP_EOL;
-                                    $content .= '商品価格　'.$item->currentPrice.PHP_EOL;
-                                    $content .= '商品サービス　wowma'.PHP_EOL;
-                                    $content .= '商品ページ '.$item->url.PHP_EOL;
-                                    $this->sendEmail($content, $user->email);
+                                    $content .= $user->name.' 様'.'<br>';
+                                    $content .= '商品があります。'.'<br>';
+                                    $content .= '商品名　'.$item->itemName.'<br>';
+                                    $content .= '商品価格　'.$item->currentPrice.'<br>';
+                                    $content .= '商品サービス　wowma'.'<br>';
+                                    $content .= '商品ページ '.$item->url.'<br>';
+                                    $this->sendEmail($content,$item->url, $user);
+                                    
                                 }
                             }
                         }
@@ -108,7 +115,7 @@ class SendNotification extends Command
                         $pages = 0;
                         // $new = mb_convert_encoding($keyword, "SJIS", "UTF-8");
                         for ($i = 0; $i < $pages + 1; $i++) {
-                            
+                            if($this->count > self::SENT_COUNT) break;
                             if($i == 0 ){
                                 $url = "https://www.2ndstreet.jp/search?keyword=".$keyword."&page=0";
                                 $crawler = $client->request('GET', $url);
@@ -121,6 +128,7 @@ class SendNotification extends Command
                                 $crawler = $client->request('GET', $url);
                             }
                             $crawler->filter('.js-favorite')->each(function ($node) {
+                                if($this->count > self::SENT_COUNT) return false;
                                 $url = $node->filter('a.listLink')->attr('href');
                                 $itemImageUrl = $node->filter('.imgBlock img')->attr('data-src');
                                 $currentPrice = intval(preg_replace('/[^0-9]+/', '', $node->filter('.price')->text()), 10);
@@ -128,13 +136,14 @@ class SendNotification extends Command
 
                                 if($this->compareCondition($this->lower_price, $this->upper_price,$this->excluded_word, $currentPrice, $itemName )){
                                     $content = '';
-                                    $content .= $user->name.' 様'.PHP_EOL;
-                                    $content .= '新商品があります。'.PHP_EOL;
-                                    $content .= '商品名　'.$itemName.PHP_EOL;
-                                    $content .= '商品価格　'.$currentPrice.PHP_EOL;
-                                    $content .= '商品サービス　wowma'.PHP_EOL;
-                                    $content .= '商品ページ '.$url.PHP_EOL;
-                                    $this->sendEmail($content, $user->email);
+                                    $content .= $this->user->name.' 様'.'<br>';
+                                    $content .= '商品があります。'.'<br>';
+                                    $content .= '商品名　'.$itemName.'<br>';
+                                    $content .= '商品価格　'.$currentPrice.'<br>';
+                                    $content .= '商品サービス　2ndstreet'.'<br>';
+                                    $content .= '商品ページ https://www.2ndstreet.jp'.$url.'<br>';
+                                    $this->sendEmail($content,$url, $this->user);
+                                   
                                 }
                             });
                         }
@@ -145,7 +154,7 @@ class SendNotification extends Command
                         $pages = 1;
                         // $new = mb_convert_encoding($keyword, "SJIS", "UTF-8");
                         for ($i = 1; $i < $pages + 1; $i++) {
-                            
+                            if($this->count > self::SENT_COUNT) break;
                             if($i == 1 ){
                                 $url = "https://komehyo.jp/search/?q=".$keyword."&page=1";
                                 $crawler = $client->request('GET', $url);
@@ -158,20 +167,21 @@ class SendNotification extends Command
                                 $crawler = $client->request('GET', $url);
                             }
                             $crawler->filter('.p-lists__item')->each(function ($node) {
-                                if($this->count > 5) return false;
+                                if($this->count > self::SENT_COUNT) return false;
                                 $url = $node->filter('a.p-link')->attr('href');
                                 $itemImageUrl = $node->filter('.p-link__head img')->attr('src');
                                 $currentPrice = intval(preg_replace('/[^0-9]+/', '', $node->filter('.p-link__txt--price')->text()), 10);
                                 $itemName   = $node->filter('.p-link__txt--productsname')->text();
                                 if($this->compareCondition($this->lower_price, $this->upper_price,$this->excluded_word, $currentPrice, $itemName )){
                                     $content = '';
-                                    $content .= $user->name.' 様'.PHP_EOL;
-                                    $content .= '新商品があります。'.PHP_EOL;
-                                    $content .= '商品名　'.$itemName.PHP_EOL;
-                                    $content .= '商品価格　'.$currentPrice.PHP_EOL;
-                                    $content .= '商品サービス　wowma'.PHP_EOL;
-                                    $content .= '商品ページ '.$url.PHP_EOL;
-                                    $this->sendEmail($content, $user->email);;
+                                    $content .= $this->user->name.' 様'.'<br>';
+                                    $content .= '商品があります。'.'<br>';
+                                    $content .= '商品名　'.$itemName.'<br>';
+                                    $content .= '商品価格　'.$currentPrice.'<br>';
+                                    $content .= '商品サービス　komehyo'.'<br>';
+                                    $content .= '商品ページ https://komehyo.jp'.$url.'<br>';
+                                    $this->sendEmail($content,$url, $this->user);
+                                    
                                 }
                                 
                             });
@@ -202,53 +212,64 @@ class SendNotification extends Command
         return $result;
     }
 
-    public function sendEmail($content, $email) {
+    public function sendEmail($content,$url, $user) {
 
-        $user_id = 'trialphoenix';
-        $api_key = '2aUSJ6gntGT6paez6XPaihMc0XEXZDWJqbwIVbRmpSWXwsDCKGjUZRDjfMIjt4Hw';
-        if ($user_id === false) {
-            echo "ユーザIDは必須です";
-            exit;
+        if(Url::where('user_id',$user->id)->where('url',$url)->count()){
+            $this->info("it is already registered");
+        }else{
+            $this->count++;
+            Url::create([
+                'user_id' => $user->id,
+                'url' => $url,
+            ]);
+            $email = $user->email;
+            $user_id = 'trialphoenix';
+            $api_key = '2aUSJ6gntGT6paez6XPaihMc0XEXZDWJqbwIVbRmpSWXwsDCKGjUZRDjfMIjt4Hw';
+            if ($user_id === false) {
+                echo "ユーザIDは必須です";
+                exit;
+            }
+            if ($api_key === false) {
+                echo "APIキーは必須です";
+                exit;
+            }
+            // トークン生成
+            $str = "$user_id$api_key";
+            $token = base64_encode(strtolower(hash('sha256', $str)));
+            // APIエンドポイント
+            $url = 'https://app.engn.jp/api/v1/deliveries/transaction';
+            // POSTデータ
+            $data = [
+                "from" => [
+                        "email" => "yuuzi2006@gmail.com",
+                        "name" => "frimawatch"
+                ],
+                "to" => $email,
+                "subject" => "商品があります。",
+                "encode" => "ISO-2022-JP",
+                "text_part" => "テスト配信",
+                "html_part" => $content
+            ];
+            $data = json_encode($data);
+    
+            // ヘッダー
+            $header = [
+                "Content-Type: application/json",
+                "Authorization: Bearer $token"
+            ];
+            // リクエスト内容を組み立て
+            $context = [
+                "http" => [
+                        "method"  => "POST",
+                        "header"  => implode("\r\n", $header),
+                        "content" => $data
+                ]
+            ];
+            // APIリクエスト
+            $res = file_get_contents($url, false, stream_context_create($context));
+            // 結果の出力
+            $this->info("sent");
         }
-        if ($api_key === false) {
-            echo "APIキーは必須です";
-            exit;
-        }
-        // トークン生成
-        $str = "$user_id$api_key";
-        $token = base64_encode(strtolower(hash('sha256', $str)));
-        // APIエンドポイント
-        $url = 'https://app.engn.jp/api/v1/deliveries/transaction';
-        // POSTデータ
-        $data = [
-            "from" => [
-                    "email" => "yuuzi2006@gmail.com",
-                    "name" => "柴原義也"
-            ],
-            "to" => $email,
-            "subject" => "通知",
-            "encode" => "ISO-2022-JP",
-            "text_part" => "テスト配信",
-            "html_part" => $content
-        ];
-        $data = json_encode($data);
-
-        // ヘッダー
-        $header = [
-            "Content-Type: application/json",
-            "Authorization: Bearer $token"
-        ];
-        // リクエスト内容を組み立て
-        $context = [
-            "http" => [
-                    "method"  => "POST",
-                    "header"  => implode("\r\n", $header),
-                    "content" => $data
-            ]
-        ];
-        // APIリクエスト
-        $res = file_get_contents($url, false, stream_context_create($context));
-        // 結果の出力
-        $this->info("sent");
+        
     }
 }
---
+++ app/Models/Url.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Url extends Model
+{
+    use HasFactory;
+
+    protected $table = 'register_url';
+
+    protected $fillable = [
+        'user_id',
+        'url',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+}
