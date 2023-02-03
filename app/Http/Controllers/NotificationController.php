755a8ca11cbd85d9734df5e895af976e3180bf3e
fandi ahmad <superdev195128@hotmail.com>
2023-02-03 17:35:29 +0900

    update

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -306,7 +306,7 @@ class NotificationController extends Controller
                     if($this->count > self::TOTAL_COUNT) break;
                     if($i == 1 ){
                         $url = "https://auctions.yahoo.co.jp/search/search?p=".$keyword."&va=".$keyword."&fixed=1&exflg=1&b=1&n=50";//ヤフオク（定額）
-                        dd($url);
+                        
                         $crawler = $client->request('GET', $url);
                         try {
                             $pages = ($crawler->filter('.Pager__lists li')->count() > 0)
